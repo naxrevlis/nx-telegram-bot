07d9d625279b2edd8827591249bcb052054cf1cb
Maksim Panin <me@naxrevlis.com>
2022-09-06 00:47:59 +0300

    initial commit

---
+++ nx_telegram_bot/config.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+
+def read_config_from_env():
+    """Read .env file to json"""
+    load_dotenv()
+    return {
+        "tg_bot_token": os.getenv("TG_NX_TOKEN"),
+        "chat_id": os.getenv("TG_NX_CHAT_ID"),
+        "host": os.getenv("RMQ_HOST"),
+        "port": os.getenv("RMQ_PORT"),
+        "user": os.getenv("RMQ_USER"),
+        "password": os.getenv("RMQ_PASSWORD"),
+        "exchange": os.getenv("RMQ_EXCHANGE"),
+        "queue": os.getenv("RMQ_QUEUE"),
+        "routing_key": os.getenv("RMQ_ROUTING_KEY"),
+    }
+
+
+def main():
+    print(read_config_from_env())
+
+
+if __name__ == "__main__":
+    main()
---
+++ nx_telegram_bot/main.py
@@ -0,0 +1,42 @@
+from email import message
+from telegram.ext import Updater
+from nx_telegram_bot.config import read_config_from_env
+import logging
+import pika
+import os
+import json
+import sys
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+
+config = read_config_from_env()
+
+updater = Updater(token=config['tg_bot_token'], use_context=True)
+
+def callback(ch, method, properties, body):
+    """Callback function"""
+    logging.info(" [x] Received %r" % body)
+    body = json.loads(body.replace(b"'", b'"'))
+    message = f"Сегодня {body['days_since_first_meet']} дней с первой встречи или {body['days_since_first_meet'] * 24} часов или {body['days_since_first_meet']//7} недель"
+    updater.bot.send_message(chat_id=config['chat_id'], text=message)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=config['host']))
+    channel = connection.channel()
+    channel.queue_declare(queue=config['queue'])
+    channel.basic_consume(queue=config['queue'], on_message_callback=callback)
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
